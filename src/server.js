c9f7717f6593a8bfb765ee5a2dff051607f8f3be
MattGueler <mateus.gueler@gmail.com>
2022-06-25 12:45:50 -0300

    Get e Post das mensagens com a limitação por QS

--- src/server.js
+++ src/server.js
@@ -20,6 +20,7 @@ server.use(express.json());
 server.use(cors());
 
 // Participantes
+
 server.post('/participants', async (req, res) => {
 
     const { name } = req.body;
@@ -71,7 +72,6 @@ server.post('/participants', async (req, res) => {
 
 })
 
-
 server.get('/participants', async (req, res) => {
 
     try {
@@ -84,11 +84,84 @@ server.get('/participants', async (req, res) => {
 })
 
 // Mensagens
+
+server.post('/messages', async (req, res) => {
+
+    const { to, text, type } = req.body;
+
+    const { user } = req.headers
+
+    const time = dayjs().format('HH:MM:ss')
+
+    const userSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+    });
+
+    const validation = userSchema.validate({
+        to: to,
+        text: text
+    }, { abortEarly: true });
+
+    if (validation.error) {
+        console.log(validation.error.details)
+        res.sendStatus(422)
+        return
+    }
+
+    if (type !== 'private_message' && type !== 'message') {
+        res.sendStatus(422)
+        console.log('erro no tipo')
+        return
+    }
+
+    const valid = await db.collection("users").findOne({
+        name: user
+    })
+    if (!valid) {
+        res.sendStatus(422)
+        console.log('erro no usuario')
+        return
+    }
+
+    try {
+
+        db.collection("messages").insertOne({
+            from: user,
+            to,
+            text,
+            type,
+            time
+        });
+
+        res.sendStatus(201)
+    }
+    catch {
+        res.sendStatus(500)
+    }
+
+})
+
+
 server.get('/messages', async (req, res) => {
+
+    const limit = parseInt(req.query.limit)
 
     try {
         const messages = await db.collection("messages").find().toArray();
-        res.send(messages)
+
+        let limitedMessages = []
+
+        if (limit < messages.length) {
+            for (let counter = 0; counter < limit; counter++) {
+                limitedMessages.push(messages[(messages.length - 1) - counter])
+            }
+        }
+        else {
+            limitedMessages = messages
+        }
+
+        res.send(limitedMessages)
     }
     catch {
         res.sendStatus(500)
